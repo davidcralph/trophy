ac929eab3c8789848ae4bfdb9a220c20ee741c8e
David Ralph <ohlookitsderpy@protonmail.com>
2019-03-10 12:08:27 +0000

    Add loader (see extended description)
    This runs when the Medal desktop app opens, injecting css/js imports into the index.html. Currently doesn't support removing plugins/themes!

---
+++ loader.js
@@ -0,0 +1,21 @@
+const { readdir } = require('fs');
+
+// Load themes
+readdir('./resources/app/trophy/themes/', (err, files) => { // Start https://anidiots.guide/first-bot/a-basic-command-handler (MODIFIED)
+    if (err) return console.log(err);
+    files.forEach(file => {
+        if (!file.endsWith(".css")) return;
+        let name = file.split(".")[0];
+        $('head').append(`<link rel="stylesheet" href="./trophy/themes/${name}.css"/>`);
+    });
+}); // End https://anidiots.guide/first-bot/a-basic-command-handler (MODIFIED)
+
+// Load plugins
+readdir('./resources/app/trophy/plugins/', (err, files) => { // Start https://anidiots.guide/first-bot/a-basic-command-handler (MODIFIED)
+    if (err) return console.log(err);
+    files.forEach(file => {
+        if (!file.endsWith(".js")) return;
+        let name = file.split(".")[0];
+        $('body').append(`<script src="./trophy/plugins/${name}.js"></script>`);
+    });
+}); // End https://anidiots.guide/first-bot/a-basic-command-handler (MODIFIED)

147cadf94f67db5c74b1f1c8570c6cd63fc9abb6
David Ralph <ohlookitsderpy@protonmail.com>
2019-03-10 10:43:54 +0000

    refactor

--- injector.js
+++ injector.js
@@ -1,12 +1,19 @@
-const { join }                = require('path');
-const copyFileSync            = require('fs-copy-file-sync');
-const { readFile, writeFile } = require('fs-cheerio');
+const { join }                                                   = require('path');
+const { load }                                                   = require('cheerio');
+const { unlinkSync, copyFileSync, writeFileSync, readFileSync }  = require('fs');
 
-(async() => {
-    const path = join(process.env.LOCALAPPDATA, 'Medal'); // This line is based on Powercord's Windows injector, which can be found at https://github.com/powercord-org/powercord/blob/v2/injectors/win32.js (MIT LICENSE)
-    const html = `${path}\\app-4.87.0\\resources\\app\\index.min.html`;
+const path = join(process.env.LOCALAPPDATA, 'Medal'); // This line is based on Powercord's Windows injector, which can be found at https://github.com/powercord-org/powercord/blob/v2/injectors/win32.js (MIT LICENSE)
+const html = `${path}\\app-4.87.0\\resources\\app\\index.min.html`;
+
+exports.inject = async () => {
     copyFileSync(html, './backup-index.min.html');
-    let $ = await readFile(html);
+    const file = readFileSync(html);
+    let $ = load(file);
     $('head').append('<link rel="stylesheet" href="./style.css" type="text/css"/>');
-    await writeFile(html, $);
-})();
+    await writeFileSync(html, $.html());
+}
+
+exports.uninject = () => {
+    copyFileSync('./backup-index.min.html', html);
+    unlinkSync('./backup-index.min.html');
+}

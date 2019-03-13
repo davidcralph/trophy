3eeada35669ea69424af724f74cbc2826ce30674
David Ralph <ohlookitsderpy@protonmail.com>
2019-03-13 10:59:51 +0000

    One does not simply merge into master
    https://whatthecommit.com/

--- loader.js
+++ loader.js
@@ -4,9 +4,9 @@ const { readdir } = require('fs');
 readdir('./resources/app/trophy/themes/', (err, files) => { // Start https://anidiots.guide/first-bot/a-basic-command-handler (MODIFIED)
     if (err) return console.log(err);
     files.forEach(file => {
-        if (!file.endsWith(".css")) return;
-        let name = file.split(".")[0];
-        $('head').append(`<link rel="stylesheet" href="./trophy/themes/${name}.css"/>`);
+        if (!file.endsWith('.css')) return;
+        let name = file.split('.')[0];
+        $('head').append(`<link rel='stylesheet' href='./trophy/themes/${name}.css'/>`);
     });
 }); // End https://anidiots.guide/first-bot/a-basic-command-handler (MODIFIED)
 
@@ -14,8 +14,8 @@ readdir('./resources/app/trophy/themes/', (err, files) => { // Start https://ani
 readdir('./resources/app/trophy/plugins/', (err, files) => { // Start https://anidiots.guide/first-bot/a-basic-command-handler (MODIFIED)
     if (err) return console.log(err);
     files.forEach(file => {
-        if (!file.endsWith(".js")) return;
-        let name = file.split(".")[0];
-        $('body').append(`<script src="./trophy/plugins/${name}.js"></script>`);
+        if (!file.endsWith('.js')) return;
+        let name = file.split('.')[0];
+        $('body').append(`<script src='./trophy/plugins/${name}.js'></script>`);
     });
 }); // End https://anidiots.guide/first-bot/a-basic-command-handler (MODIFIED)

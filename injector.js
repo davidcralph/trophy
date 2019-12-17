ab104a171be21285c45f25d595b5bade93013aad
David Ralph <ohlookitsderpy@protonmail.com>
2019-12-17 13:34:00 +0000

    Update injector.js

--- injector.js
+++ injector.js
@@ -1,29 +1,28 @@
-const { sync }                                                             = require('rimraf');
-const { join }                                                             = require('path');
-const { load }                                                             = require('cheerio');
-const { unlinkSync, copyFileSync, writeFileSync, readFileSync, mkdirSync } = require('fs');
+const rimraf = require('rimraf');
+const path = require('path');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
-const path = join(process.env.LOCALAPPDATA, 'Medal'); // This line is based on Powercord's Windows injector, which can be found at https://github.com/powercord-org/powercord/blob/v2/injectors/win32.js (MIT LICENSE)
-const app  = `${path}\\app-4.273.0\\resources\\app`;
+const app  = `${path.join(process.env.LOCALAPPDATA, 'Medal')}\\app-4.273.0\\resources\\app`;
 
-exports.inject = async () => {
+exports.inject = () => {
     // Create backup
-    copyFileSync(`${app}\\index.min.html`, './backup-index.min.html');
+    fs.copyFileSync(`${app}\\index.min.html`, './backup-index.min.html');
     // Create directories and copy loader
-    mkdirSync(`${app}\\trophy`);
-    mkdirSync(`${app}\\trophy\\themes`);
-    mkdirSync(`${app}\\trophy\\plugins`);
-    copyFileSync('./loader.js', `${app}\\trophy\\loader.js`);
+    fs.mkdirSync(`${app}\\trophy`);
+    fs.mkdirSync(`${app}\\trophy\\themes`);
+    fs.mkdirSync(`${app}\\trophy\\plugins`);
+    fs.copyFileSync('./loader.js', `${app}\\trophy\\loader.js`);
     // Inject into the Medal HTML
-    const file = readFileSync(`${app}\\index.min.html`);
-    let $ = load(file);
+    const file = fs.readFileSync(`${app}\\index.min.html`);
+    let $ = cheerio.load(file);
     $('body').append('<script src=\'./trophy/loader.js\'></script>');
-    await writeFileSync(`${app}\\index.min.html`, $.html());
+    writeFileSync(`${app}\\index.min.html`, $.html());
 }
 
 exports.uninject = () => {
     // Uninject everything and remove backup
-    copyFileSync('./backup-index.min.html', `${app}\\index.min.html`);
-    unlinkSync('./backup-index.min.html');
-    sync(`${app}\\trophy`);
+    fs.copyFileSync('./backup-index.min.html', `${app}\\index.min.html`);
+    fs.unlinkSync('./backup-index.min.html');
+    rimraf.sync(`${app}\\trophy`);
 }
